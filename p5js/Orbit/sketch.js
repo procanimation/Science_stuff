36b266a88ca03aa3376ec5da581ced3541623d69
Procanimation <93392263+procanimation@users.noreply.github.com>
2022-10-22 14:56:21 +0530

    Added space for readability

--- p5js/Orbit/sketch.js
+++ p5js/Orbit/sketch.js
@@ -9,16 +9,20 @@ function setup() {
 function draw(){
    background(51);
    push();
+
    ellipseMode(CENTER);
    translate(width/2, height/2);
+   
    stroke(221);
    noFill();
    ellipse(0, 0, 2*a, 2*b);
    fill(231);
+
    let c = int(sqrt(a*a - b*b)) 
 
    ellipse(-c, 0, 100, 100);
    ellipse(a*cos(theta), b*sin(theta), 50, 50);
+
    pop();
    if(theta <= -360){
       theta = 0;
